3ba01f4aeb078d236d5e49a7c7c251c9e6d1fd51
Komal borge <kborge26@gmail.com>
2022-12-30 15:45:54 +0530

    angular packages
    angular packages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AuthGuard } from './auth.guard';
+import { ContactusComponent } from './contactus/contactus.component';
+import { DemopostComponent } from './demopost/demopost.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PostdetailsComponent } from './postdetails/postdetails.component';
+import { LaptopComponent } from './product/laptop/laptop.component';
+import { ProductComponent } from './product/product.component';
+import { TabletComponent } from './product/tablet/tablet.component';
+import { TvComponent } from './product/tv/tv.component';
+import { WashingmachineComponent } from './product/washingmachine/washingmachine.component';
+
+const routes: Routes = [
+   //{path:'', redirectTo: 'login', pathMatch:'full'},
+   //{path:'login', component:LoginComponent},
+   //{path:'home', component:HomeComponent},
+   //{path:'aboutus', component:AboutusComponent},
+   //{path:'contactus', component:ContactusComponent},
+   //{path:'product', component:ProductComponent},
+   //{path:'**', component:PagenotfoundComponent}
+
+
+   //{path:'', redirectTo: 'login', pathMatch:'full'},
+   //{path:'login', component:LoginComponent},
+   //{path:'home', component:HomeComponent},
+   //{path:'aboutus', component:AboutusComponent},//
+   //{path:'contactus', component:ContactusComponent},
+   //{path:'product', children: [
+     //{path:'',component:ProductComponent},
+     //{path:'laptop',component:LaptopComponent},
+    //{path:'tv',component:TvComponent},
+    //{path:'tablet',component:TabletComponent},
+   // {path:'washingmachine',component:WashingmachineComponent}
+   //]},
+   //{path:'**', component:PagenotfoundComponent}
+
+
+
+  {path:'', redirectTo: 'login', pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'home', component:HomeComponent},
+  {path:'aboutus', component:AboutusComponent},
+  {path:'contactus', component:ContactusComponent},
+  {path:'product',canActivate:[AuthGuard], component:ProductComponent, children: [
+    {path:'',component:ProductComponent},
+    {path:'laptop',component:LaptopComponent},
+    {path:'tv',component:TvComponent},
+    {path:'tablet',component:TabletComponent},
+    {path:'washingmachine',component:WashingmachineComponent}
+  ]},
+{path:'post', component: DemopostComponent},
+  {path:'postdetails/id', component:PostdetailsComponent},// to go on perticular page
+  {path:'**', component:PagenotfoundComponent}
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
